874813761985f05eaef4a9a436b80b9bd2f187f3
Necrolemun <56665440+Necrolemun@users.noreply.github.com>
2019-10-17 22:56:27 +0800

    Update AbityIrnituBot.py
    Добавлены новые кнопки

--- AbityIrnituBot.py
+++ AbityIrnituBot.py
@@ -8,51 +8,64 @@ bot = telebot.TeleBot('981156430:AAFLH8w6tIWGkaXC5iPkU7CMXvYI2R4uS8M')
 updater = Updater(token='981156430:AAFLH8w6tIWGkaXC5iPkU7CMXvYI2R4uS8M') 
 dispatcher = updater.dispatcher
 
-
-
 @bot.message_handler(commands=["start"])
 def start(m):
 	msg = bot.send_message(m.chat.id, "Вас приветствует AbityBot помощь /help")
 	keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
-	keyboard.add(*[types.KeyboardButton(name) for name in ['СПО']])
-	keyboard.add(*[types.KeyboardButton(name) for name in ['Бакалавриат, специалитет']])
-	keyboard.add(*[types.KeyboardButton(name) for name in ['Магистратура']])
-	keyboard.add(*[types.KeyboardButton(name) for name in ['Аспирантура']])
-	bot.send_message(m.chat.id, 'На какой уровень образования собираешься поступать?',
+	keyboard.add(*[types.KeyboardButton(name) for name in ['Информация о направлениях']])
+	keyboard.add(*[types.KeyboardButton(name) for name in ['Подобрать специальность']])
+	keyboard.add(*[types.KeyboardButton(name) for name in ['Часто задаваемые вопросы']])
+	bot.send_message(m.chat.id, 'Выбери что тебе нужно.',
 		reply_markup=keyboard)
 	bot.register_next_step_handler(msg, name)
 
+
 def name(m):
+	if m.text == 'Информация о направлениях':
+		keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+		keyboard.add(*[types.KeyboardButton(name) for name in ['СПО']])
+		keyboard.add(*[types.KeyboardButton(name) for name in ['Бакалавриат, специалитет']])
+		keyboard.add(*[types.KeyboardButton(name) for name in ['Магистратура']])
+		keyboard.add(*[types.KeyboardButton(name) for name in ['Аспирантура']])
+		msg = bot.send_message(m.chat.id, 'На какой уровень образования собираешься поступать?',
+			reply_markup=keyboard)
+		bot.register_next_step_handler(msg, name1)
+	elif m.text == 'Часто задаваемые вопросы':
+		keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+		bot.send_message(m.chat.id, 'Введите /v + вопрос',
+			reply_markup=keyboard)
+
+def name1(m):
 	if m.text == 'СПО':
 		keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
 		keyboard.add(*[types.KeyboardButton(name) for name in ['Очная СПО']])
 		msg = bot.send_message(m.chat.id, 'Форма обучения?',
 			reply_markup=keyboard)
-		bot.register_next_step_handler(msg, name1)
+		bot.register_next_step_handler(msg, name2)
 	elif m.text == 'Бакалавриат, специалитет':
 		keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
 		keyboard.add(*[types.KeyboardButton(name) for name in ['Очная Бакалавриат, специалитет']])
 		keyboard.add(*[types.KeyboardButton(name) for name in ['Заочная Бакалавриат, специалитет']])
 		msg = bot.send_message(m.chat.id, 'Форма обучения?',
 			reply_markup=keyboard)
-		bot.register_next_step_handler(msg, name1)
+		bot.register_next_step_handler(msg, name2)
 	elif m.text == 'Магистратура':
 		keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
 		keyboard.add(*[types.KeyboardButton(name) for name in ['Очная Магистратура']])
 		keyboard.add(*[types.KeyboardButton(name) for name in ['Заочная Магистратура']])
 		msg = bot.send_message(m.chat.id, 'Форма обучения?',
 			reply_markup=keyboard)
-		bot.register_next_step_handler(msg, name1)
+		bot.register_next_step_handler(msg, name2)
 	elif m.text == 'Аспирантура':
 		keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
 		keyboard.add(*[types.KeyboardButton(name) for name in ['Очная Аспирантура']])
 		keyboard.add(*[types.KeyboardButton(name) for name in ['Заочная Аспирантура']])
 		msg = bot.send_message(m.chat.id, 'Форма обучения?',
 			reply_markup=keyboard)
-		bot.register_next_step_handler(msg, name1) #отправляем в name1
+		bot.register_next_step_handler(msg, name2) #отправляем в name1
 	print(m.text)
 #направление в name1
-def name1(m):
+def name2(m):
 	print(m.text)
 	if m.text == 'Очная Аспирантура':
 		keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
